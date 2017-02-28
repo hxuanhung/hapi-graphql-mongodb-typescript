e5e97a12a7c7342e42833254d69b09cc410caa3e
hxhung <hxuanhung@gmail.com>
2017-02-28 16:38:12 +0100

    feat(message): Provides CRUD on Message model

--- src/modules/message/Message.ts
+++ src/modules/message/Message.ts
@@ -6,7 +6,48 @@ export class Message {
 		return res ? res : []
 	}
 
+	getMessageById(id: string) {
+		return MessageModel.findById(id, (err, doc) => {
+			console.log(err, doc);
+			if (err) {
+				console.log("Something wrong when get data!");
+				return err;
+			}
+
+			return doc;
+		})
+	}
+
 	create(text: string) {
-		return new MessageModel({ text: text }).save();
+		return new MessageModel({ text: text }).save((err, doc) => {
+			console.log(err, doc);
+			if (err) {
+				console.log("Something wrong when create data!");
+				return err;
+			}
+
+			return doc;
+		});
+	}
+
+	modify(id: string, text: string) {
+		return MessageModel.findByIdAndUpdate(id, { text: text }, { new: true }, (err, doc) => {
+			if (err) {
+				console.log("Something wrong when updating data!");
+				return err;
+			}
+
+			return doc;
+		})
+	}
+
+	delete(id: string) {
+		return MessageModel.findByIdAndRemove(id, (err, doc) => {
+			if (err) {
+				console.log("Something wrong when delete data!");
+				return err;
+			}
+			return 'Deleted';
+		})
 	}
 }
